26c91c9d8ce235943b67fe66c15f6467a04cd387
SergioGBosco <sergiogibosco@gmail.com>
2025-07-04 13:30:25 +0200

    feat: add variable for add an email

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@
 
 const email = [`sergio@boolean.it`, `dinosauro@libero.it`, `diobuonoegeneroso@gmail.com`, `aspettoilpranzo@hofame.it`]
 
-//
+// credo una variabile e-mail per l'utente
 
+useremail = prompt(`Inserisci la tua e-mail per la verifica`)
+
+//genero un ciclo for per controllare che l'email inserita dall'utente è presente all'interno della lista degli invitati 
+
+for (let i = 0; i < email.length; i++) {
+
+}
+console.log(useremail)
