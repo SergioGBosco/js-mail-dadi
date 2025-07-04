1604430593ba97cd143bb8fa9575c8a984a10616
SergioGBosco <sergiogibosco@gmail.com>
2025-07-04 13:17:52 +0200

    feat: add array e-mail

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Esercizio Email
+// Creo un array con all'interno delle e-mail
+
+const email = [`sergio@boolean.it`, `dinosauro@libero.it`, `diobuonoegeneroso@gmail.com`, `aspettoilpranzo@hofame.it`]
+
+//
+
